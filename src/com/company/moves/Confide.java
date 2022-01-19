0c6c0ec29fa3f4f4115e7300921460ae021cdcca
fsaidov11 <fsaidov11@mail.ru>
2022-01-19 18:17:10 +0300

    #7 : fixed Confide effect.

--- src/com/company/moves/Confide.java
+++ src/com/company/moves/Confide.java
@@ -10,9 +10,7 @@ public class Confide extends StatusMove {
     @Override
     protected void applyOppEffects(Pokemon p) {
         Effect e1 = new Effect().turns(1).stat(Stat.SPECIAL_ATTACK, -1);
-        Effect e2 = new Effect().turns(1).stat(Stat.HP, 6);
         p.addEffect(e1);
-        p.addEffect(e2);
     }
 
     @Override
